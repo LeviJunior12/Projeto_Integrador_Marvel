3fb5366601d8c65cff43ccef1f5bf2824deaaf3c
Henrique <henriquecouto@poli.ufrj.br>
2020-11-22 00:46:12 -0300

    Ajustes de UI

--- app/src/main/java/com/example/projetointegradormarvel/list/ListFragment.kt
+++ app/src/main/java/com/example/projetointegradormarvel/list/ListFragment.kt
@@ -1,12 +1,11 @@
 package com.example.projetointegradormarvel.list
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projetointegradormarvel.R
 import com.example.projetointegradormarvel.characters.CharacterAdapter
@@ -33,7 +32,7 @@ class ListFragment : Fragment() {
 //        super.onViewCreated(view, savedInstanceState)
 //    }
 
-    fun getListItems(): ArrayList <Characters>{
+    private fun getListItems(): ArrayList<Characters> {
         return arrayListOf(
             Characters(R.drawable.ic_marvel_logo),
             Characters(R.drawable.ic_launcher_foreground),
