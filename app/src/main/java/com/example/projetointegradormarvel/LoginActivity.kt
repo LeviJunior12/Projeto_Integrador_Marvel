31a6408502b37bfd03256a28970c3c61d807a2c5
Henrique <henriquecouto@poli.ufrj.br>
2021-01-28 14:07:57 -0300

    Finaliza auth Firebase

--- app/src/main/java/com/example/projetointegradormarvel/LoginActivity.kt
+++ app/src/main/java/com/example/projetointegradormarvel/LoginActivity.kt
@@ -7,7 +7,10 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.example.projetointegradormarvel.databinding.ActivityLoginBinding
-import com.facebook.*
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -20,7 +23,6 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity(), View.OnClickListener {
 
@@ -89,49 +91,13 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
         }
     }
 
-    private fun createAccount(email: String, password: String) {
-        Log.d(TAG, "createAccount:$email")
-        if (!validateForm()) {
-            return
-        }
-
-        showProgressBar()
-
-        // [START create_user_with_email]
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
-                    val user = auth.currentUser
-//                    updateUI(user)
-                    callHome(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-//                    updateUI(null)
-                }
-
-                // [START_EXCLUDE]
-                hideProgressBar()
-                // [END_EXCLUDE]
-            }
-        // [END create_user_with_email]
-    }
-
     private fun signIn(email: String, password: String) {
         Log.d(TAG, "signIn:$email")
         if (!validateForm()) {
             return
         }
-
         showProgressBar()
 
-        // [START sign_in_with_email]
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
@@ -139,7 +105,6 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
                     Log.d(TAG, "signInWithEmail:success")
                     val user = auth.currentUser
                     callHome(user)
-//                    updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithEmail:failure", task.exception)
@@ -147,29 +112,22 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
                         baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT
                     ).show()
-//                    updateUI(null)
-                    // [START_EXCLUDE]
 //                    checkForMultiFactorFailure(task.exception!!)
-                    // [END_EXCLUDE]
                 }
-
-                // [START_EXCLUDE]
 //                if (!task.isSuccessful) {
 //                    binding.status.setText(R.string.auth_failed)
 //                }
                 hideProgressBar()
-                // [END_EXCLUDE]
             }
-        // [END sign_in_with_email]
     }
-
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        // Result returned from Facebook
         callbackManager.onActivityResult(requestCode, resultCode, data)
 
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        // Result returned from Google
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
@@ -180,18 +138,15 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
             } catch (e: ApiException) {
                 // Google Sign In failed, update UI appropriately
                 Log.w(TAG, "Google sign in failed", e)
-                // [START_EXCLUDE]
-//                updateUI(null)
-                // [END_EXCLUDE]
             }
         }
     }
 
     // Auth with google
     private fun firebaseAuthWithGoogle(idToken: String) {
-        // [START_EXCLUDE silent]
+
         showProgressBar()
-        // [END_EXCLUDE]
+
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
@@ -200,32 +155,25 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
                     Log.d(TAG, "signInWithCredential:success")
                     val user = auth.currentUser
                     callHome(user)
-//                    updateUI(user)
+
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
-                    // [START_EXCLUDE]
 //                    val view = binding.mainLayout
-                    // [END_EXCLUDE]
                     Toast.makeText(
                         baseContext, "Google Authentication Failed",
                         Toast.LENGTH_SHORT
                     ).show()
-//                    Snackbar.make(view, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
 //                    updateUI(null)
                 }
-
-                // [START_EXCLUDE]
                 hideProgressBar()
-                // [END_EXCLUDE]
             }
     }
 
     private fun handleFacebookAccessToken(token: AccessToken) {
         Log.d(TAG, "handleFacebookAccessToken:$token")
-        // [START_EXCLUDE silent]
+
         showProgressBar()
-        // [END_EXCLUDE]
 
         val credential = FacebookAuthProvider.getCredential(token.token)
         auth.signInWithCredential(credential)
@@ -235,7 +183,7 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
                     Log.d(TAG, "signInWithCredential:success")
                     val user = auth.currentUser
                     callHome(user)
-//                    updateUI(user)
+
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
@@ -243,81 +191,31 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
                         baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT
                     ).show()
-//                    updateUI(null)
                 }
-
-                // [START_EXCLUDE]
                 hideProgressBar()
-                // [END_EXCLUDE]
             }
     }
-    // [END auth_with_facebook]
-
 
     private fun signInGoogle() {
         val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
-    private fun signOut() {
-        auth.signOut()
-//        updateUI
-
-//        callLogin
-    }
-
-/*    private fun sendEmailVerification() {
-        // Disable button
-        binding.verifyEmailButton.isEnabled = false
-
-        // Send verification email
-        // [START send_email_verification]
-        val user = auth.currentUser!!
-        user.sendEmailVerification()
-            .addOnCompleteListener(this) { task ->
-                // [START_EXCLUDE]
-                // Re-enable button
-                binding.verifyEmailButton.isEnabled = true
-
-                if (task.isSuccessful) {
-                    Toast.makeText(baseContext,
-                        "Verification email sent to ${user.email} ",
-                        Toast.LENGTH_SHORT).show()
-                } else {
-                    Log.e(TAG, "sendEmailVerification", task.exception)
-                    Toast.makeText(baseContext,
-                        "Failed to send verification email.",
-                        Toast.LENGTH_SHORT).show()
-                }
-                // [END_EXCLUDE]
-            }
-        // [END send_email_verification]
-    }*/
-
     private fun reload() {
         auth.currentUser!!.reload().addOnCompleteListener { task ->
             if (task.isSuccessful) {
 //                updateUI(auth.currentUser)
-                Toast.makeText(
-                    this@LoginActivity,
-                    "Reload successful!",
-                    Toast.LENGTH_SHORT
-                ).show()
+                Log.e(TAG, "reload")
             } else {
                 Log.e(TAG, "reload", task.exception)
-                Toast.makeText(
-                    this@LoginActivity,
-                    "Failed to reload user.",
-                    Toast.LENGTH_SHORT
-                ).show()
             }
         }
     }
 
     private fun validateForm(): Boolean {
         var valid = true
-
         val email = binding.edEmail.text.toString()
+
         if (TextUtils.isEmpty(email)) {
             binding.edEmail.error = "Required"
             valid = false
@@ -332,98 +230,32 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
         } else {
             binding.edPassword.error = null
         }
-
         return valid
     }
 
-//    private fun updateUI(user: FirebaseUser?) {
-//        hideProgressBar()
-//        if (user != null) {
-//            binding.status.text = getString(R.string.emailpassword_status_fmt,
-//                user.email, user.isEmailVerified)
-//            binding.detail.text = getString(R.string.firebase_status_fmt, user.uid)
-//
-//            binding.emailPasswordButtons.visibility = View.GONE
-//            binding.emailPasswordFields.visibility = View.GONE
-//            binding.signedInButtons.visibility = View.VISIBLE
-//
-//            if (user.isEmailVerified) {
-//                binding.verifyEmailButton.visibility = View.GONE
-//            } else {
-//                binding.verifyEmailButton.visibility = View.VISIBLE
-//            }
-//        } else {
-//            binding.status.setText(R.string.signed_out)
-//            binding.detail.text = null
-//
-//            binding.emailPasswordButtons.visibility = View.VISIBLE
-//            binding.emailPasswordFields.visibility = View.VISIBLE
-//            binding.signedInButtons.visibility = View.GONE
-//        }
-//    }
-
-/*    private fun checkForMultiFactorFailure(e: Exception) {
-        // Multi-factor authentication with SMS is currently only available for
-        // Google Cloud Identity Platform projects. For more information:
-        // https://cloud.google.com/identity-platform/docs/android/mfa
-        if (e is FirebaseAuthMultiFactorException) {
-            Log.w(TAG, "multiFactorFailure", e)
-            val intent = Intent()
-            val resolver = e.resolver
-            intent.putExtra("EXTRA_MFA_RESOLVER", resolver)
-            setResult(MultiFactorActivity.RESULT_NEEDS_MFA_SIGN_IN, intent)
-            finish()
-        }
-    }*/
-
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.btnSignUp -> {
-                createAccount(binding.edEmail.text.toString(), binding.edPassword.text.toString())
-            }
+            R.id.btnSignUp -> callSignup()
             R.id.btnLogIn -> signIn(
                 binding.edEmail.text.toString(),
                 binding.edPassword.text.toString()
             )
             R.id.btnGoogle -> signInGoogle()
-//            R.id.signOutButton -> signOut()
-//            R.id.verifyEmailButton -> sendEmailVerification()
-//            R.id.reloadButton -> reload()
         }
     }
 
-    fun callHome(user: FirebaseUser?) {
+    private fun callHome(user: FirebaseUser?) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 
+    private fun callSignup() {
+        var intent = Intent(this, SignupActivity::class.java)
+        startActivity(intent)
+    }
+
     companion object {
-        private const val TAG = "EmailPassword"
-        private const val RC_MULTI_FACTOR = 9005
+        const val TAG = "LoginActivity"
         private const val RC_SIGN_IN = 9001
     }
 }
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.z_activity_login)
-//
-//        btn_login.setOnClickListener {
-//            callHome()
-//        }
-//
-//        btn_cadastro.setOnClickListener {
-//            callCadastro()
-//        }
-//    }
-//
-//    fun callHome() {
-//        var intent = Intent(this, MainActivity::class.java)
-//        startActivity(intent)
-//    }
-//
-//    fun callCadastro() {
-//        var intent = Intent(this, z_SignupActivity::class.java)
-//        startActivity(intent)
-//    }
-//}
--- app/src/main/java/com/example/projetointegradormarvel/SignupActivity.kt
+++ app/src/main/java/com/example/projetointegradormarvel/SignupActivity.kt
@@ -1,34 +1,106 @@
 package com.example.projetointegradormarvel
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_signup.*
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.example.projetointegradormarvel.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class SignupActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class SignupActivity : BaseActivity(), View.OnClickListener {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignupBinding
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
 
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setProgressBar(binding.progressBar)
 
-        btn_confCadastro.setOnClickListener {
-            callHome()
+        binding.btnCreateAct.setOnClickListener(this)
+        binding.btnCancel.setOnClickListener(this)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+    }
+
+    private fun createAccount(email: String, password: String) {
+        Log.d(LoginActivity.TAG, "createAccount:$email")
+        if (!validateForm()) {
+            return
         }
 
-        btn_cancelaCad.setOnClickListener {
-            callLogin()
+        showProgressBar()
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(LoginActivity.TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    callHome(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(LoginActivity.TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Failed to create",
+                        Toast.LENGTH_SHORT
+                    ).show()
+//                    updateUI(null)
+                }
+                hideProgressBar()
+            }
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+        val email = binding.edEmail.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            binding.edEmail.error = "Required"
+            valid = false
+        } else {
+            binding.edEmail.error = null
+        }
+
+        val password = binding.edPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            binding.edPassword.error = "Required"
+            valid = false
+        } else {
+            binding.edPassword.error = null
+        }
+        return valid
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnCreateAct -> createAccount(
+                binding.edEmail.text.toString(),
+                binding.edPassword.text.toString()
+            )
+            R.id.btnCancel -> callLogin()
         }
     }
 
-
-
-    fun callHome(){
-        var intent = Intent(this, MainActivity::class.java)
+    private fun callHome(user: FirebaseUser?) {
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 
-    fun callLogin(){
-        var intent = Intent(this, LoginActivity::class.java)
+    private fun callLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
+    }
+
+    companion object {
+        const val TAG = "SignupActivity"
     }
 }
